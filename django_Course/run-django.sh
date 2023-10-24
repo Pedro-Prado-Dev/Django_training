8b757766e2f2c1803f6d286d8f98f0b845fb7544
Pedro_Prado <pedro.paiva@inatel.br>
2023-10-24 09:12:29 -0300

    Django-Training-class-07

--- django_Course/run-django.sh
+++ django_Course/run-django.sh
@@ -2,5 +2,12 @@
 
 cd /code
 
+until python test_database.py; do
+  echo "Database is unavailable - sleeping"
+  sleep 1
+done
+
 python manage.py migrate --settings=proj.settings_production
+python manage.py collectstatic --no-input
 python manage.py runserver 0.0.0.0:8000 --settings=proj.settings_production
+#gunicorn proj.wsgi:application --env DJANGO_SETTINGS_MODULE=proj.settings_production --limit-request-line 0 --timeout 600 -w 4 -b :8000
