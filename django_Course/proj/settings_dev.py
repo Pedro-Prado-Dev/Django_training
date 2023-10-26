4ca18013cfaf25d3581cfa5175344add4bca2ee5
Pedro_Prado <pedro.paiva@inatel.br>
2023-10-26 09:30:35 -0300

    Django-Training-class-08

--- django_Course/proj/settings_dev.py
+++ django_Course/proj/settings_dev.py
@@ -1,7 +1,9 @@
 from .settings import *
+
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-INTERNAL_IPS = ('127.0.0.1', 'localhost')
+INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -12,6 +14,27 @@ DATABASES = {
         'PORT': '5433',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'sidia',
+#         'CLIENT': {
+#                 'host': '127.0.0.1',
+#                 'port': 27016,
+#                 'username': 'root',
+#                 'password': 'sidia',
+#             },
+#     }
+# }
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27016
+MONGO_USER = 'root'
+MONGO_PASSWORD = 'sidia'
+MONGO_DB = 'tickets'
+
 AUTH_PASSWORD_VALIDATORS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'Suporte  <pedroppaiva1@hotmail.com>'
--- django_Course/proj/settings_production.py
+++ django_Course/proj/settings_production.py
@@ -2,14 +2,30 @@ from .settings import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-INTERNAL_IPS = ('127.0.0.1', 'localhost')
+INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sidia',
         'USER': 'postgres',
         'PASSWORD': 'root',
-        'HOST': 'bd',
+        'HOST': 'db_prod',
         'PORT': '5432',
     }
 }
+
+
+MONGO_HOST = 'mongo_db'
+MONGO_PORT = 27017
+MONGO_USER = 'root'
+MONGO_PASSWORD = 'sidia'
+MONGO_DB = 'tickets'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'suporte@sidia.com.br'
+EMAIL_HOST_PASSWORD = 'senha'
+DEFAULT_FROM_EMAIL = 'Suporte  <pedroppaiva1@hotmail.com>'
